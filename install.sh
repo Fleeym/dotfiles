e7fe09a117ced5fc290d351157bc0a8fbfe8e58e
Gabriel Badea <gabi.badea313@gmail.com>
2022-03-27 16:34:05 +0300

    Added oh my zsh installation

--- install.sh
+++ install.sh
@@ -17,3 +17,15 @@ git config --global user.name "$full_name"
 
 echo "Git config done!"
 echo "---------------------------"
+echo "2) zsh Install"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Oh My ZSH! installed"
+echo "---------------------------"
+echo "3) Powerlevel10k"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+cp .zshrc ~/.zshrc
+p10k configure
+
+echo "Powerlevel10k installed!"
