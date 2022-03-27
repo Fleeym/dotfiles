e4b31fc25b9e98edea1768a8e2a9c5046bfc92f2
Gabriel Badea <gabi.badea313@gmail.com>
2022-03-27 11:11:01 +0300

    Add install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "WELCOME TO FLAME'S DOTFILES!"
+echo "---------------------------"
+echo "1) Git Config"
+
+echo "Enter your first and last name: "
+read full_name
+
+echo "Type in your email adress: "
+read email
+
+git config --global user.email "$email"
+git config --global user.name "$full_name"
+
+echo "Git config done!"
+echo "---------------------------"
