4fd0832a610ce45ca5e4a55b7cb0f7f6e5d5c9f5
Fleeym <61891787+Fleeym@users.noreply.github.com>
2023-07-26 20:59:00 +0300

    add lualine

--- nvim/lua/flame/packer.lua
+++ nvim/lua/flame/packer.lua
@@ -91,4 +91,8 @@ return require('packer').startup(function(use)
             require('gitsigns').setup()
         end
     }
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
 end)
